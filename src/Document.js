8cf542b9451948c041c9a964efeb0a8b6e220f54
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-15 15:58:27 +0300

    fixes

--- src/Document.js
+++ src/Document.js
@@ -1,20 +1,25 @@
 
-import useScrollBottom from "react-scroll-bottom-hook";
-
-
-
+import {  useRef } from "react";
+import { useState} from "react/cjs/react.development";
 
 export default function Document(props){
-
-   const [scrolled, setScroll] = useScrollBottom();
+  const [scrolled, setScrolled] = useState(false);
+   const div = useRef(null);
   
+    const scrolledDown = () => {
+      if((div.current.scrollTop + div.current.clientHeight) >= div.current.scrollHeight - 1) {
+        setScrolled(true);
+      } 
+    }
+  
+   
     return (
       <>
-      <h1 className="title" value={"Terms and Conditions"}>Terms and Conditions</h1>
-      <div className="content" style={{overflowY: 'scroll'}} ref={setScroll} >{props.txt}</div>
-      <button disabled={!scrolled}>I Agree</button>
+      <h1 className="title" >{props.title}</h1>
+      <div className="content" onScrollCapture={scrolledDown} style={{overflowY: 'scroll'}} ref={div} >{props.content}</div>
+      <button disabled={!scrolled} >I Agree</button>
       </>
     )
     
   }   
-  
+ 
