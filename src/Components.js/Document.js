fc38b3233c9643bf94d19902ce364dedc448bb68
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-15 19:30:49 +0300

    fixes

--- src/Components.js/Document.js
+++ src/Components.js/Document.js
@@ -2,21 +2,23 @@
 import {  useRef } from "react";
 import { useState} from "react/cjs/react.development";
 
-export default function Document(props) {
+export default function Document({title, content}) {
   const [scrolled, setScrolled] = useState(false);
    const div = useRef(null);
   
     const scrolledDown = () => {
       if((div.current.scrollTop + div.current.clientHeight) >= div.current.scrollHeight - 1) {
         setScrolled(true);
-      } 
+      } else {
+        setScrolled(false)
+      }
     }
   
    
     return (
       <>
-      <h1 className="title" >{props.title}</h1>
-      <div className="content" onScrollCapture={scrolledDown} style={{overflowY: 'scroll'}} ref={div} >{props.content}</div>
+      <h1 className=".title" >{title}</h1>
+      <div className="content" onScrollCapture={scrolledDown} style={{overflowY: 'scroll'}} ref={div} >{content}</div>
       <button disabled={!scrolled} >I Agree</button>
       </>
     )
